4e8ea220abe63802bbf312da39c03906a864c3f8
Carlos Del Rosario <carlosdelros@gmail.com>
2016-02-07 17:15:03 -0500

    added layers to the map

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function init() {
     maxZoom: 3,
     minZoom: 1,
     crs: L.CRS.Simple
-  }).setView([000,0], 1);
+  }).setView([0,0], 1);
 
   map.setMaxBounds(new L.LatLngBounds([0,510], [550,0]));
 
@@ -11,6 +11,15 @@ function init() {
   var imageBounds = [[500,0], [0,500]];
 
   L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+//[south,west], [north,east]
+var redBounds = [[233,230], [279,275]];
+var hotRectangle = L.rectangle(redBounds,{color:'#f00',fillOpacity: 0.5}).addTo(map);
+
+//[south,west], [north,east]
+var blueBounds = [[233,275], [279,320]];
+var coldRectangle = L.rectangle(blueBounds,{color:'#00f',fillOpacity: 0.5}).addTo(map);
+  
 }
 
 init();
