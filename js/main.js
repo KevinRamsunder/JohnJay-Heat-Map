86d4d18fe85db97bbd9998c61260e3adf996e5ba
Kevin Ramsunder <kevinramsunder5@gmail.com>
2016-02-07 14:11:42 -0500

    Added logo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 function init() {
-
   var map = L.map('map', {
-      maxZoom: 3,
-      minZoom: 1,
-      crs: L.CRS.Simple
-  }).setView([0, 0], 1);
+    maxZoom: 3,
+    minZoom: 1,
+    crs: L.CRS.Simple
+  }).setView([000,0], 1);
 
-  map.setMaxBounds(new L.LatLngBounds([0,550], [500,0]));
+  map.setMaxBounds(new L.LatLngBounds([0,510], [550,0]));
 
   var imageUrl = '../images/10th floor.jpg'
   var imageBounds = [[500,0], [0,500]];
