1e2efa60a41cd7699a71cb111814c714f6a8239c
Carlos Del Rosario <carlosdelros@gmail.com>
2016-02-08 00:32:44 -0500

    added more layers

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function init() {
+//function init() {
   var map = L.map('map', {
     maxZoom: 3.6,
     minZoom: 1,
@@ -7,7 +7,6 @@ function init() {
     crs: L.CRS.Simple
   });
 
-  
   var imageUrl = '../images/10th floor.jpg';
   var width=2109,height=2271;
 
@@ -17,23 +16,18 @@ function init() {
 
   L.imageOverlay(imageUrl, bounds).addTo(map);
   map.setMaxBounds(bounds);
-
-  console.log(southWest+" "+northEast);
   
-  var redBounds = [[-71,104], [-52.5,148]];
-  var redRectangle = L.rectangle(redBounds,{color:'#f00',fillOpacity: 0.5}).addTo(map);
 
-  var blueBounds = [[-71,148.5], [-52.5,192.5]];
-  var blueRectangle = L.rectangle(blueBounds,{color:'#00f',fillOpacity: 0.5}).addTo(map);
+  var layers = new Array();
 
-  var yellowBounds = [[-71,193], [-52.5,237]];
-  var yellowRectangle = L.rectangle(yellowBounds,{color:'#ff0',fillOpacity: 0.5}).addTo(map);
+  layers.push(L.polygon([[-52.5,75.5],[-95.5,75.5],[-95.5,94],[-71,94],[-71,103],[-52.5,103]],{color:'#0f0',fillOpacity: 0.5}).addTo(map));
+  layers.push(L.rectangle([[-71,104], [-52.5,148]],{color:'#f00',fillOpacity: 0.5}).addTo(map));
+  layers.push(L.rectangle([[-71,148.5], [-52.5,192.5]],{color:'#00f',fillOpacity: 0.5}).addTo(map));
+  layers.push(L.rectangle([[-71,193], [-52.5,237]],{color:'#ff0',fillOpacity: 0.5}).addTo(map));
+  layers.push(L.rectangle([[-71,238], [-52.5,281]],{color:'#0f0',fillOpacity: 0.5}).addTo(map));
+  layers.push(L.rectangle([[-80.5,282], [-52.5,315]],{color:'#f0f',fillOpacity: 0.5}).addTo(map))
+  layers.push(L.rectangle([[-135,75.5], [-96,94]],{color:'#f0f',fillOpacity: 0.5}).addTo(map));
+  layers.push(L.rectangle([[-165,75.5], [-136,94]],{color:'#3f8',fillOpacity: 0.5}).addTo(map));
+//}
 
-  var greenBounds = [[-71,238], [-52.5,281]];
-  var greentRectangle = L.rectangle(greenBounds,{color:'#0f0',fillOpacity: 0.5}).addTo(map);
-
-  var polygon = L.polygon([[-52.5,75.5],[-95.5,75.5],[-95.5,94],[-71,94],[-71,103],[-52.5,103]],{color:'#0f0',fillOpacity: 0.5}).addTo(map);
-
-}
-
-init();
+//init();
