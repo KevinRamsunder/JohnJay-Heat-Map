ce9b0f273e0707e0aab8972d9c8e24e9a155a112
Alessandro DiMarco <axdimarco@gmail.com>
2016-02-21 19:24:38 -0500

    Add more rooms to floor 10 json

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ function init() {
   var map = L.map('map', {
     maxZoom: 3.6,
     minZoom: 1,
-    center:[-165,308],//DONT FORGET TO CHANGE THIS TO 0,0
-    zoom:1,//DONT FORGET TO CHANGE THIS TO 1
+    center:[-390,150],//DONT FORGET TO CHANGE THIS TO 0,0
+    zoom:2,//DONT FORGET TO CHANGE THIS TO 1
     crs: L.CRS.Simple
   });
 
@@ -21,42 +21,24 @@ function init() {
 
   var layers = new Array();
 
-  // #B6D079
+  for (var i in data) {
+    rand_color = "#"+((1<<24)*Math.random()|0).toString(16)
+    if (data[i].length == 2)
+      layers.push(L.rectangle(data[i],{color:rand_color,fillOpacity: 0.7}).addTo(map));
+    else{
+      layers.push(L.polygon(data[i],{color:rand_color,fillOpacity: 0.7}).addTo(map));
+    }
+  }
 
-  layers.push(L.polygon(data["10.65.06"],{color:'red',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle(data["10.65.07"],{color:'blue',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle(data["10.65.08"],{color:'red',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-71,148.5], [-52.5,192.5]],{color:'blue',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-71,193], [-52.5,237]],{color:'yellow',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-71,238], [-52.5,281]],{color:'black',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-80.5,282], [-52.5,315]],{color:'orange',fillOpacity: 0.5}).addTo(map))
-  layers.push(L.rectangle([[-135,75.5], [-96,94]],{color:'aqua',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-165,75.5], [-136,94]],{color:'purple',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.polygon([[-80.5,105],[-94.5,105],[-94.5,133],[-118,133],[-118,144.5],[-109,145],[-109,166],[-80.5,166]],{color:'magenta',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-150,104.5], [-119,144.5]],{color:'peru',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-108,176.5], [-80.5,218]],{color:'red',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-108.5,228], [-80.5,270]],{color:'green',fillOpacity: 0.5}).addTo(map));
+  //right side
   layers.push(L.polygon([[-81.5,281],[-318,281],[-318,315],[-235,315],[-235,308],[-164.5,308],[-164.5,315],[-81.5,315]],{color:'blue',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-199.5,161.5], [-165.5,189.5]],{color:'yellow',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-199.5,191], [-165.5,222.5]],{color:'black',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-199.5,223], [-165.5,241.5]],{color:'orange',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-234.5,242.5], [-165.5,265]],{color:'aqua',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-234.5,161.5], [-200.5,190]],{color:'purple',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-234.5,191], [-200.5,222.5]],{color:'magenta',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-234.5,223], [-200.5,241.5]],{color:'peru',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-264,75], [-235,94]],{color:'red',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-303.5,75], [-265,94]],{color:'green',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-291.5,104], [-250,145]],{color:'blue',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.polygon([[-260.75,168],[-290,168],[-290,190.5],[-279,191],[-279,195],[-268,195],[-268,176],[-260.75,176]],{color:'yellow',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.polygon([[-292.5,104.5],[-319,104.5],[-319,166],[-291,166],[-291,146],[-293,146]],{color:'black',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-319.5,176.5], [-291,218]],{color:'orange',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-319.5,228], [-291,270]],{color:'aqua',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.polygon([[-304,75],[-347.5,75],[-347.5,103],[-329,103],[-329,94],[-304,94]],{color:'purple',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-347.5,104], [-329,147.5]],{color:'magenta',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-347.5,148.5], [-329,192.5]],{color:'peru',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-347.5,193.5], [-329,236.5]],{color:'red',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-347.5,237.5], [-329,281.5]],{color:'green',fillOpacity: 0.5}).addTo(map));
-  layers.push(L.rectangle([[-347.5,282], [-319,314.5]],{color:'yellow',fillOpacity: 0.5}).addTo(map));
+
+  //10.T.02 Mens Toilet
+  layers.push(L.polygon([[-260.75,168],[-290,168],[-290,190.5],[-279,191],[-279,195],[-268,195],[-268,176],[-260.75,176]],{color:'blue',fillOpacity: 0.5}).addTo(map));
+
+  // stair doesnt have room number
+  // layers.push(L.rectangle([[-234.5,223], [-200.5,241.5]],{color:'peru',fillOpacity: 0.5}).addTo(map));
+
 
   var info = L.control();
   info.onAdd = function (map) {
