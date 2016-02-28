42a6b737321e95a4165891b29f930328170536b1
Kevin Ramsunder <kevinramsunder5@gmail.com>
2016-02-28 15:08:34 -0500

    Added vector shapes to the map

--- public/app/controllers/MainController.js
+++ public/app/controllers/MainController.js
@@ -52,12 +52,42 @@ var initMap = function(self) {
     });
 };
 
-var addShapes = function(leafletData, $http, shapes) {
-    leafletData.getMap('map').then(function(map) {
-        $http.get('api/v1/rooms/47112').then(function(response) {
-            console.log(response);
+var addShapes = function(leafletData, $http, $scope) {
+    // function to make http call to get content of JSON
+    var getRoomDataFromJson = new Promise(function(resolve, reject) {
+        $http.get('/json/floor_10/room_num.json').then(function(response) {
+            var roomNumbers = response;
+
+            $http.get('/json/floor_10/vav.json').then(function(response) {
+                resolve({'roomNumbers': roomNumbers, 'vavBoxes': response});
+            })
         });
-        // make json call
-        // add layer for every json
+    });
+
+    // go through object and add vector shapes to map
+    var addShapesToMap = function(map, roomNumbers, vavBoxes) {
+        for(var vav in vavBoxes) {
+            for(var i = 0; i < vavBoxes[vav].length; i++) {
+                var coordinates = roomNumbers[vavBoxes[vav][i]];
+
+                var rand_color = "#" + ((1 << 24) * Math.random() | 0).toString(16)
+                var object = {color: rand_color, fillOpacity: .5};
+                
+                if(coordinates.length === 2) {
+                    map.addLayer(new L.rectangle(coordinates, object));
+                } else {
+                    map.addLayer(new L.polygon(coordinates, object));
+                }
+            }
+        }
+    };
+
+    // execute
+    leafletData.getMap('map').then(function(map) {
+        var data = getRoomDataFromJson.then(function(response) {
+            var roomNumbers = response.roomNumbers.data;
+            var vavBoxes = response.vavBoxes.data;
+            addShapesToMap(map, roomNumbers, vavBoxes);
+        });
     });
 };
