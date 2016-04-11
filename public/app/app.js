4f5609aa80f984f8d0877c9a6c3d80e5a39ff972
Alessandro DiMarco <axdimarco@gmail.com>
2016-04-10 20:59:30 -0400

    Removed if statment for polygon

--- public/app/app.js
+++ public/app/app.js
@@ -32,21 +32,34 @@ app.service('mapInteraction', function() {
                 fillOpacity: .5
             };
 
-            if (coordinates.length === 2) {
-                // var layer = new L.rectangle(coordinates, object);
-                var latlng = L.latLng((coordinates[0][0]+coordinates[1][0])/2, (coordinates[0][1]+coordinates[1][1])/2);
-                if(currentTemp !== undefined) {
-                    var layer = new L.circle(latlng, (currentTemp * 300), object);
-                } else {
-                    var layer = new L.circle(latlng, 10000, object);
-                }
+            // var layer = new L.rectangle(coordinates, object);
+            var latlng = L.latLng((coordinates[0][0]+coordinates[1][0])/2, (coordinates[0][1]+coordinates[1][1])/2);
+            if(currentTemp !== undefined) {
+                var layer = new L.circle(latlng, (currentTemp * 300), object);
             } else {
-                var layer = new L.polygon(coordinates, object);
+                var layer = new L.circle(latlng, 10000, object);
             }
 
             if (!self.vectorLayers.hasOwnProperty(vav)) {
                 self.vectorLayers[vav] = [];
             }
+
+            // Too see coordinates
+            // var info = L.control();
+            // info.onAdd = function (map) {
+            //     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+            //     return this._div;
+            // };
+            //
+            // info.update = function (latlong) {
+            //     this._div.innerHTML = latlong;
+            // };
+            //
+            // map.on('mousemove',function(e){
+            //     info.update(e.latlng);
+            // });
+            //
+            // info.addTo(map);
 
             map.addLayer(layer);
             self.vectorLayers[vav].push(layer);
