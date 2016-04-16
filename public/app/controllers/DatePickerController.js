cc832fb2e4ba4b02a8a807754f16e2bd76caa9ef
Alessandro DiMarco <axdimarco@gmail.com>
2016-04-16 12:32:01 -0400

    Switch names for end and start dates

--- public/app/controllers/DatePickerController.js
+++ public/app/controllers/DatePickerController.js
@@ -2,27 +2,13 @@
 app.controller('DatePickerController', datePickerController);
 
 // inject dependencies into 'MainController' controller
-datePickerController.$inject = ['$scope', '$http'];
+datePickerController.$inject = ['$scope', '$http', 'datePickerService'];
 
 // controller function
-function datePickerController($scope, $http) {
-    $scope.leftDate = new Date(2016, 0, 5);
-    $scope.rightDate = new Date(2016, 0, 27);
+function datePickerController($scope, $http, datePickerService) {
+    $scope.startDate = new Date(2016, 0, 5);
+    $scope.endDate   = new Date(2016, 0, 27);
 
-    $scope.leftMinDate = new Date(2013, 6, 6);
-    $scope.leftMaxDate = $scope.rightDate;
-    $scope.rightMinDate = $scope.leftDate;
-    $scope.rightMaxDate = new Date(2016, 1, 31);
-
-
-    $scope.checkDate = function() {
-        if ($scope.leftDate.getTime() !== $scope.rightMinDate.getTime()) {
-            $scope.rightMinDate = $scope.leftDate;
-        }
-
-        if ($scope.rightDate.getTime() !== $scope.leftMaxDate.getTime()) {
-            $scope.leftMaxDate = $scope.rightDate;
-        }
-    };
-
+    $scope.minDate = new Date(2013, 6, 6);
+    $scope.maxDate = new Date(2016, 1, 31);
 }
