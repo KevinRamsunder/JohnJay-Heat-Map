5969a6778f2f3dae022a0a4b01cfae7a5a1fd048
Alessandro DiMarco <axdimarco@gmail.com>
2016-04-16 15:21:39 -0400

    Add updateDate function

--- public/app/controllers/DatePickerController.js
+++ public/app/controllers/DatePickerController.js
@@ -2,16 +2,21 @@
 app.controller('DatePickerController', datePickerController);
 
 // inject dependencies into 'MainController' controller
-datePickerController.$inject = ['$scope', '$http', 'datePickerService'];
+datePickerController.$inject = ['$scope', 'datePickerService'];
 
 // controller function
-function datePickerController($scope, $http, datePickerService) {
+function datePickerController($scope, datePickerService) {
     $scope.startDate = new Date(2016, 0, 5);
     $scope.endDate   = new Date(2016, 0, 27);
 
-    $scope.minDate = new Date(2013, 6, 6);
+    $scope.minDate = new Date(2013, 5, 6);
     $scope.maxDate = new Date(2016, 1, 31);
 
-    datePickerService.startDate = $scope.startDate;
-    datePickerService.endDate = $scope.endDate;
+    $scope.updateDate = function () {
+        datePickerService.startDate = $scope.startDate;
+        datePickerService.endDate = $scope.endDate;
+        datePickerService.dateChanged = true;
+    };
+
+    $scope.updateDate();
 }
