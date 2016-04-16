3ab2594ddea7ee9eca076e5a29e300f62823e5c8
Alessandro DiMarco <axdimarco@gmail.com>
2016-04-16 18:56:03 -0400

    Change start and end date

--- public/app/controllers/DatePickerController.js
+++ public/app/controllers/DatePickerController.js
@@ -3,8 +3,8 @@ app.controller('DatePickerController', datePickerController);
 datePickerController.$inject = ['$scope', 'datePickerService'];
 
 function datePickerController($scope, datePickerService) {
-    $scope.startDate = new Date(2016, 0, 5);
-    $scope.endDate   = new Date(2016, 0, 27);
+    $scope.startDate = new Date(2016, 0, 1);
+    $scope.endDate   = new Date(2016, 0, 31);
 
     $scope.minDate = new Date(2013, 5, 6);
     $scope.maxDate = new Date(2016, 1, 31);
