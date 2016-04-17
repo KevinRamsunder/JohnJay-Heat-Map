a671d8e6c76d3525f90f67658ae20e30681779f0
Alessandro DiMarco <axdimarco@gmail.com>
2016-04-17 00:47:01 -0400

    Update end date

--- public/app/controllers/DatePickerController.js
+++ public/app/controllers/DatePickerController.js
@@ -4,10 +4,10 @@ datePickerController.$inject = ['$scope', 'datePickerService'];
 
 function datePickerController($scope, datePickerService) {
     $scope.startDate = new Date(2016, 0, 1);
-    $scope.endDate   = new Date(2016, 0, 31);
+    $scope.endDate   = new Date(2016, 0, 30);
 
     $scope.minDate = new Date(2013, 5, 6);
-    $scope.maxDate = new Date(2016, 1, 31);
+    $scope.maxDate = new Date(2016, 0, 31);
 
     $scope.updateDate = function () {
         datePickerService.startDate = $scope.startDate;
