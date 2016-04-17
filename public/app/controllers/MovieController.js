6d2200fbc45cec653d4c4947970868476b1b1687
Alessandro DiMarco <axdimarco@gmail.com>
2016-04-17 01:20:39 -0400

    Reformat file

--- public/app/controllers/MovieController.js
+++ public/app/controllers/MovieController.js
@@ -3,14 +3,14 @@ app.controller('MovieController', movieController);
 movieController.$inject = ['$scope', '$http', '$interval', 'leafletData', 'tableToMapService', 'mapInteraction', 'datePickerService'];
 
 function movieController($scope, $http, $interval, leafletData, tableToMapService, mapInteraction, datePickerService) {
-    $scope.showDate  = 'Current Date';
+    $scope.showDate = 'Current Date';
     $scope.isStopped = true;
     $scope.animation = undefined;
-    $scope.interval  = 50; // refresh rate for animation
+    $scope.interval = 50; // refresh rate for animation
 
     // index for run movie
     $scope.startDateIndex = 0;
-    $scope.endDateIndex   = 0;
+    $scope.endDateIndex = 0;
 
     // variables for markers and data being shown
     $scope.marker_type = 'Circles';
@@ -22,18 +22,18 @@ function movieController($scope, $http, $interval, leafletData, tableToMapServic
     // list of dates [2013-06-06 00:00:00", "2013-06-06 01:00:00", ...]
     $scope.currentFloorDates = [];
 
-    $scope.getData = function() {
+    $scope.getData = function () {
         mapInteraction.makingRequest = true;
 
-        $http.get('app/assets/json/floor_10/room_num.json').then(function(response) {
+        $http.get('app/assets/json/floor_10/room_num.json').then(function (response) {
             $scope.roomNumbers = response.data;
         });
 
-        $http.get('app/assets/json/floor_10/vav.json').then(function(response) {
+        $http.get('app/assets/json/floor_10/vav.json').then(function (response) {
             $scope.vavs = response.data;
         });
 
-        $http.get('/api/v1/rooms').then(function(response) {
+        $http.get('/api/v1/rooms').then(function (response) {
             // object with {47102: "2013-06-06 00:00:00, 73.13, 2013-06006 01:00:00, 73.0, ...}
             // vav: string of all data
             var masterData = response.data;
@@ -41,13 +41,15 @@ function movieController($scope, $http, $interval, leafletData, tableToMapServic
             var tempData = {};
             var do_once = true;
 
-            for(var key in masterData) {
+            for (var key in masterData) {
                 tempData[key] = masterData[key].split(',');
-                tempData[key] = tempData[key].map(function(i) {return i.trim()});
+                tempData[key] = tempData[key].map(function (i) {
+                    return i.trim()
+                });
 
                 var floorData = {};
-                for(var i = 0; i < tempData[key].length-1; i += 2) {
-                    floorData[tempData[key][i]] = tempData[key][i+1];
+                for (var i = 0; i < tempData[key].length - 1; i += 2) {
+                    floorData[tempData[key][i]] = tempData[key][i + 1];
 
                     if (do_once) {
                         $scope.currentFloorDates.push(tempData[key][i])
@@ -63,41 +65,39 @@ function movieController($scope, $http, $interval, leafletData, tableToMapServic
         });
     };
 
-    $scope.startAnimation = function() {
+    $scope.startAnimation = function () {
         if (datePickerService.dateChanged || $scope.startDateIndex >= $scope.endDateIndex) {
             $scope.setDateIndex();
         }
 
         $scope.isStopped = false;
 
-        leafletData.getMap('map').then(function(map) {
+        leafletData.getMap('map').then(function (map) {
             $scope.animate(map);
         });
     };
 
-    $scope.stopAnimation = function() {
-        if(angular.isDefined($scope.animation)) {
+    $scope.stopAnimation = function () {
+        if (angular.isDefined($scope.animation)) {
             $scope.isStopped = true;
             $interval.cancel($scope.animation);
             $scope.animation = undefined;
         }
     };
 
-    $scope.restartAnimation = function() {
+    $scope.restartAnimation = function () {
         $scope.setDateIndex();
         $scope.startAnimation();
     };
 
-    $scope.animate = function(map) {
-        $scope.animation = $interval(function() {
+    $scope.animate = function (map) {
+        $scope.animation = $interval(function () {
             var current_date = $scope.currentFloorDates[$scope.startDateIndex];
 
-
-
-            for(var vav in $scope.vavs) {
+            for (var vav in $scope.vavs) {
 
                 if (current_date in $scope.currentFloorData[vav]) {
-                    var temp  = $scope.currentFloorData[vav][current_date];
+                    var temp = $scope.currentFloorData[vav][current_date];
                     var color = tableToMapService.getColorFromRanges(temp).color;
 
                     $scope.showDate = current_date;
@@ -110,19 +110,19 @@ function movieController($scope, $http, $interval, leafletData, tableToMapServic
 
             $scope.startDateIndex += 1;
 
-            if($scope.startDateIndex > $scope.endDateIndex) {
+            if ($scope.startDateIndex > $scope.endDateIndex) {
                 $scope.stopAnimation();
             }
         }, $scope.interval);
     };
 
-    $scope.loaderStatus = function() {
+    $scope.loaderStatus = function () {
         return mapInteraction.loading || mapInteraction.makingRequest;
     };
 
-    $scope.setDateIndex = function() {
-        var startDate = datePickerService.startDate.toISOString().substring(0,10) + ' 00:00:00';
-        var endDate = datePickerService.endDate.toISOString().substring(0,10) + ' 23:00:00';
+    $scope.setDateIndex = function () {
+        var startDate = datePickerService.startDate.toISOString().substring(0, 10) + ' 00:00:00';
+        var endDate = datePickerService.endDate.toISOString().substring(0, 10) + ' 23:00:00';
 
         $scope.startDateIndex = $scope.currentFloorDates.indexOf(startDate);
         $scope.endDateIndex = $scope.currentFloorDates.indexOf(endDate);
@@ -130,7 +130,7 @@ function movieController($scope, $http, $interval, leafletData, tableToMapServic
         datePickerService.dateChanged = false;
     };
 
-    $scope.updateMarkers = function() {
+    $scope.updateMarkers = function () {
         mapInteraction.marker_type = $scope.marker_type;
         mapInteraction.marker_options = $scope.marker_options;
     };
