94bfd88be6d302576983178b05477fc5d45dc04e
Alessandro DiMarco <axdimarco@gmail.com>
2016-04-23 18:07:51 -0400

    Update DatePicker

--- public/app/controllers/DatePickerController.js
+++ public/app/controllers/DatePickerController.js
@@ -3,8 +3,8 @@ app.controller('DatePickerController', datePickerController);
 datePickerController.$inject = ['$scope', 'DateService'];
 
 function datePickerController($scope, DateService) {
-    $scope.startDate = DateService.startDate;
-    $scope.endDate   = DateService.endDate;
+    $scope.startDate = new Date(2016, 0, 1);
+    $scope.endDate   = new Date(2016, 0, 30);
 
     $scope.minDate = new Date(2013, 5, 6);
     $scope.maxDate = new Date(2016, 0, 30);
--- public/app/services/DateService.js
+++ public/app/services/DateService.js
@@ -1,18 +1,18 @@
 app.service('DateService', function (floorDataService) {
     var self = this;
 
-    self.startDate = new Date(2016, 0, 1);
-    self.endDate = new Date(2016, 0, 30);
+    self.startDate = undefined;
+    self.endDate = undefined;
 
     self.dateChanged = false;
 
-    // returns the index of the date in the array of dates
+    // returns the index of the start date in the array of dates
     self.getStartDate = function () {
         var startDateString = self.startDate.toISOString().substring(0, 10) + ' 00:00:00';
         return floorDataService.currentFloorDates.indexOf(startDateString);
     };
 
-    // returns the index of the date in the array of dates
+    // returns the index of the end date in the array of dates
     self.getEndDate = function () {
         var endDateString = self.endDate.toISOString().substring(0, 10) + ' 23:00:00';
         return floorDataService.currentFloorDates.indexOf(endDateString);
