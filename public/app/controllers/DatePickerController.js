3995eb7f13cb3b9d2a4b7452411a30868356893f
Alessandro DiMarco <axdimarco@gmail.com>
2016-04-24 01:38:28 -0400

    Fix ending date

--- public/app/controllers/DatePickerController.js
+++ public/app/controllers/DatePickerController.js
@@ -4,10 +4,10 @@ datePickerController.$inject = ['$scope', 'DateService'];
 
 function datePickerController($scope, DateService) {
     $scope.startDate = new Date(2016, 0, 1);
-    $scope.endDate   = new Date(2016, 0, 30);
+    $scope.endDate   = new Date(2016, 0, 31);
 
     $scope.minDate = new Date(2013, 5, 6);
-    $scope.maxDate = new Date(2016, 0, 30);
+    $scope.maxDate = new Date(2016, 0, 31);
 
     $scope.updateDate = function () {
         DateService.startDate = $scope.startDate;
