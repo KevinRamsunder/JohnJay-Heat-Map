46cbd67db87a5496a66405f5220f289be4cd9ac5
Alessandro DiMarco <axdimarco@gmail.com>
2016-04-24 01:33:25 -0400

    Improve circles on zoom

--- public/app/services/MapInteractionService.js
+++ public/app/services/MapInteractionService.js
@@ -64,7 +64,16 @@ app.service('MapInteractionService', function (TableToMapService, FloorDataServi
 
         if (self.marker_type === 'Circles') {
             var latlng = L.latLng((coordinates[0][0] + coordinates[1][0]) / 2, (coordinates[0][1] + coordinates[1][1]) / 2);
-            var radius = ((TableToMapService.getIndexOfColor(color) + 1) * 5) * (zoom + 1);
+
+            // if zoom == 0
+            var radius = ((TableToMapService.getIndexOfColor(color) + 1) * 5) * .6;
+
+            if (zoom == 1) {
+                radius = ((TableToMapService.getIndexOfColor(color) + 1) * 5) * 1.2;
+            } else if (zoom == 2) {
+                radius = ((TableToMapService.getIndexOfColor(color) + 1) * 5) * 2.4;
+            }
+
 
             return new L.circleMarker(latlng, object).setRadius(radius).bindPopup(currentTemp + degreeSign);
 
