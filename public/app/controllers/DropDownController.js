26127d90a2e48db9cda921b7d4547901025d2290
Alessandro DiMarco <axdimarco@gmail.com>
2016-04-24 23:51:54 -0400

    Add tabs

--- public/app/controllers/DropDownController.js
+++ public/app/controllers/DropDownController.js
@@ -3,6 +3,11 @@ app.controller('DropDownController', dropDownController);
 dropDownController.$inject = ['$scope', 'MapInteractionService', 'LoadingService', '$rootScope'];
 
 function dropDownController($scope, MapInteractionService, LoadingService, $rootScope) {
+    $scope.tabs = [
+        { title:'Squares', content:'tab1.html' },
+        { title:'Circles', content:'tab2.html'}
+    ];
+
     // variables for markers and data being shown
     $rootScope.marker_type = 'Squares';
     $rootScope.marker_options = 'Temp';
